63fed4f6241194de3e99abaaf9f895c0859512f0
Mustapha EZZALI <mustapha_ezzali@um5.ac.ma>
2022-09-16 08:23:52 +0100

    change urls

--- backend/archibotpro/archibotpro/urls.py
+++ backend/archibotpro/archibotpro/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apiCandidat/', include('apiCandidat.urls')),
+    path('', include('apiCandidat.urls')),
 ]
